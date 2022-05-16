c6357c4c0d8650e32f4590e8010aa74ab1d5daed
Ivan <andvan2016@mail.ru>
2022-05-16 21:24:33 +0700

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String meow;
+
+    public String getMeow() {
+        return meow;
+    }
+
+    public void setMeow(String meow) {
+        this.meow = meow;
+    }
+}
